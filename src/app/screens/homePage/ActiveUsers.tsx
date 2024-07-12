b217d5592455c4a3078110950735060411420bb1
tttemurbek <nonemenoneme9999@gmail.com>
2024-07-12 19:19:12 +0900

    feat: develop newdishes, activeUsers and events components

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -1,11 +1,60 @@
 import React from "react";
 
-import { Container } from "@mui/material";
+import Card from "@mui/joy/Card";
+import CardCover from "@mui/joy/CardCover";
+import CardContent from "@mui/joy/CardContent";
+import Typography from "@mui/joy/Typography";
+import { CssVarsProvider } from "@mui/joy/styles";
+import { CardOverflow } from "@mui/joy";
+import { Visibility } from "@mui/icons-material";
+import { Box, Button, Container, Stack } from "@mui/material";
+import { DescriptionOutlined } from "@mui/icons-material";
+
+const activeUsers = [
+  { memberNick: "Martin", imagePath: "/img/martin.webp" },
+  { memberNick: "Justin", imagePath: "/img/justin.webp" },
+  { memberNick: "Rose", imagePath: "/img/rose.webp" },
+  { memberNick: "Nusret", imagePath: "/img/nusret.webp" },
+];
 
 export default function ActiveUsers() {
   return (
-    <div>
-      <Container>ActiveUsers</Container>
+    <div className={"active-users-frame"}>
+      <Container>
+        <Stack className={"main"}>
+          <Box className={"category-title"}>Popular Dishes</Box>
+          <Stack className={"cards-frame"}>
+            {activeUsers.length !== 0 ? (
+              activeUsers.map((ele, index) => {
+                return (
+                  <CssVarsProvider key={index}>
+                    <Card className={"card"} sx={{ maxWidth: 345 }}>
+                      <CardCover>
+                        <img src={ele.imagePath} alt="" />
+                      </CardCover>
+
+                      <CardContent sx={{ justifyContent: "flex-end" }}>
+                        <Stack
+                          style={{ background: "#ffff" }}
+                          flexDirection={"row"}
+                          justifyContent={"center"}
+                          marginBottom={"-35px"}
+                        >
+                          <Typography className={"member-nickname"}>
+                            {ele.memberNick}
+                          </Typography>
+                        </Stack>
+                      </CardContent>
+                    </Card>
+                  </CssVarsProvider>
+                );
+              })
+            ) : (
+              <Box className="no-data">New Products are not available!</Box>
+            )}
+          </Stack>
+        </Stack>
+      </Container>
     </div>
   );
 }
--- src/app/screens/homePage/NewDishes.tsx
+++ src/app/screens/homePage/NewDishes.tsx
@@ -1,11 +1,73 @@
 import React from "react";
 
-import { Container } from "@mui/material";
+import Card from "@mui/joy/Card";
+import CardCover from "@mui/joy/CardCover";
+import CardContent from "@mui/joy/CardContent";
+import Typography from "@mui/joy/Typography";
+import { CssVarsProvider } from "@mui/joy/styles";
+import { Box, Button, Container, Stack } from "@mui/material";
+import AspectRatio from "@mui/joy/AspectRatio";
+import { Visibility } from "@mui/icons-material";
+import { DescriptionOutlined } from "@mui/icons-material";
+import Divider from "../../components/divider";
+import CardOverflow from "@mui/joy/CardOverflow";
+
+const newDishes = [
+  { productName: "Cutlet", imagePath: "/img/cutlet.webp" },
+  { productName: "Kebab", imagePath: "/img/kebab.webp" },
+  { productName: "Kebab", imagePath: "/img/kebab-fresh.webp" },
+  { productName: "Lavash", imagePath: "/img/lavash.webp" },
+];
 
 export default function NewDishes() {
   return (
-    <div>
-      <Container>NewDishes</Container>
+    <div className={"new-products-frame"}>
+      <Container>
+        <Stack className={"main"}>
+          <Box className={"category-title"}>Fresh Menu</Box>
+          <Stack className={"cards-frame"}>
+            <CssVarsProvider>
+              {newDishes.length !== 0 ? (
+                newDishes.map((ele, index) => {
+                  return (
+                    <Card key={index} variant="outlined" className={"card"}>
+                      <CardOverflow>
+                        <div className="product-sale">Normal size</div>
+                        <AspectRatio ratio="1">
+                          <img src={ele.imagePath} alt="" />
+                        </AspectRatio>
+                      </CardOverflow>
+
+                      <CardOverflow variant="soft" className=" product-detail">
+                        <Stack className="info">
+                          <Stack flexDirection={"row"}>
+                            <Typography className={"title"}>
+                              {ele.productName}
+                            </Typography>
+                            <Divider width="2" height="24" bg="#d9d9d9" />
+                            <Typography className={"price"}>$12</Typography>
+                          </Stack>
+                          <Stack>
+                            <Typography className={"views"}>
+                              20
+                              <Visibility
+                                sx={{ fontSize: 20, marginLeft: "5px" }}
+                              />
+                            </Typography>
+                          </Stack>
+                        </Stack>
+                      </CardOverflow>
+                    </Card>
+                  );
+                })
+              ) : (
+                <Box className="no-data">New Products are not available</Box>
+              )}
+            </CssVarsProvider>
+          </Stack>
+        </Stack>
+      </Container>
+      ;
     </div>
   );
 }

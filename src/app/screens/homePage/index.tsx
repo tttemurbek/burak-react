500aaf3de2ec74b6245c464e4ca6e652df5fa71b
tttemurbek <nonemenoneme9999@gmail.com>
2024-07-29 00:42:07 +0900

    feat: create homePage Redux config

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Statistics from "./Statistics";
 import PopularDishes from "./PopularDishes";
 import NewDishes from "./NewDishes";
@@ -8,6 +8,13 @@ import Events from "./Events";
 import "../../../css/home.css";
 
 export default function HomePage() {
+  // Selector: Store => Data
+
+  useEffect(() => {
+    //  backend serverimizdan datani request qilamiz => DATA FETCH
+    // Slice: Data => Store(Redux Store desekte boladi)
+  }, []);
+
   return (
     <div className="homepage">
       <Statistics />

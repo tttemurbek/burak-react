b5b23da2d04a803d92fd35dce1f55ff8e8fb60bc
tttemurbek <nonemenoneme9999@gmail.com>
2024-07-28 15:30:33 +0900

    fix: refactoring

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -1,14 +1,29 @@
 import { Box, Button, Container, Stack } from "@mui/material";
 import { NavLink } from "react-router-dom";
 import Basket from "./Basket";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { log } from "console";
 
 export default function HomeNavbar() {
   const authMember = null;
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
+  const [value, setValue] = useState<boolean>(true);
 
-  const buttonHandler = () => {
+  useEffect(() => {
+    console.log("componentDidMount", count); // DATA FETCH
     setCount(count + 1);
+
+    return () => {
+      console.log("componentWillUnmount");
+    };
+  }, [value]);
+  //useEffect faqat bir marta ishga tushadi
+  // array valuega ma'lum bir value kiritsak, qayta ishga tushadi
+  // value qiymati ozgarganda componentDidUpdate ishga tushadi
+
+  // HANDLERS
+  const buttonHandler = () => {
+    setValue(!value);
   };
 
   return (
